266134a881147c3becb8a3f209376e77b9159e76
Luis Alejandro Vieira <lavz24@gmail.com>
2014-08-22 16:17:16 -0430

    Added reset buton

---
+++ Assets/MemoriaGame/Scripts/Utils/ReloadScene.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReloadScene : MonoBehaviour {
+
+	// Use this for initialization
+	public void Reload () {
+        Application.LoadLevel (Application.loadedLevelName);
+
+	}
+	
+	
+}
