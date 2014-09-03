70df139612dfee63f2913c5221f1e63e80a49227
Luis Alejandro Vieira <lavz24@gmail.com>
2014-09-03 14:42:19 -0430

    Sistema de sonido

--- Assets/MemoriaGame/Scripts/GUI/ReloadSceneGUI.cs
+++ Assets/MemoriaGame/Scripts/GUI/ReloadSceneGUI.cs
@@ -9,26 +9,45 @@ public class ReloadSceneGUI : MonoBehaviour {
     public UI2DSpriteAnimation spriteAnimNo;
 
     public UIWidget objectFunc;
+    public UIWidget objectSound;
 
 
     bool isPressedNo = false;
-    // Use this for initialization
-    public  void Reload () {
 
+    bool reset = false;
+    bool sound  = false;
+
+    void activeBG(){
         spriteBG.alpha = 1;
 
         spriteAnim.ResetToBeginning ();
 
         spriteAnim.Play ();
 
+    }
+    #region StartButton
+    // Use this for initialization
+    public  void Reload () {
+
+        activeBG ();
+        reset = true;
 
     }
+    public void SoundTouch(){
+        activeBG ();
+
+        sound = true;
+    }
+    #endregion
 
     public void ReloadYes(){
+        //CAMIBAR
+        PlayerPrefs.Save ();
         Application.LoadLevel (Application.loadedLevelName);
 
     }
 
+    #region BackButon:
     public void ReloadNo(){
 
         spriteAnimNo.ResetToBeginning ();
@@ -36,22 +55,55 @@ public class ReloadSceneGUI : MonoBehaviour {
 
         objectFunc.alpha = 0;
 
-
         isPressedNo = true;
     }
 
+ 
+    public void SoundTouchEnd(){
+        spriteAnimNo.ResetToBeginning ();
+        spriteAnimNo.Play ();
+
+        objectSound.alpha = 0;
+
+        isPressedNo = true;
+
+
+    }
+    #endregion
     void Update(){
     
         if (spriteBG.alpha > 0) {
         
-            if (isPressedNo) {
-                if (!spriteAnimNo.isPlaying) {
-                    spriteBG.alpha = 0;
-                    isPressedNo = false;
-                }
-            }else if (!spriteAnim.isPlaying) {
-                objectFunc.alpha = 1;
+            if (reset) {
+                ResetUpdate ();
+            } else if (sound) {
+                SoundUpdate ();
             }
+        }
+    }
+    void SoundUpdate(){
+        if (isPressedNo) {
+            if (!spriteAnimNo.isPlaying) {
+                spriteBG.alpha = 0;
+                isPressedNo = false;
+                sound = false;
+            }
+        }
+        else if (!spriteAnim.isPlaying) {
+            objectSound.alpha = 1;
+        }
+    }
+    void ResetUpdate(){
+        if (isPressedNo) {
+            if (!spriteAnimNo.isPlaying) {
+                spriteBG.alpha = 0;
+                isPressedNo = false;
+                reset = false;
+
+            }
+        }
+        else if (!spriteAnim.isPlaying) {
+            objectFunc.alpha = 1;
         }
     }
 }
---
+++ Assets/MemoriaGame/Scripts/Managers/ManagerSound.cs
@@ -0,0 +1,54 @@
+﻿//
+//  ManagerSound.cs
+//
+//  Author:
+//       Luis Alejandro Vieira <lavz24@gmail.com>
+//
+using UnityEngine;
+
+public class ManagerSound : Singleton<ManagerSound>
+{
+    [Range (0,1)]
+    public float bgVolume = 1;
+    [Range (0,1)]
+    public float fxVolume = 1;
+
+
+    public bool mute = false;
+
+    AudioSource bgAudio;
+
+    void Awake(){
+    
+        bgAudio = GameObject.FindGameObjectWithTag ("BgSound").GetComponent<AudioSource>();
+    }
+
+
+    void OnEnable() {
+        //Aqui cargo
+        bgVolume = PlayerPrefs.GetFloat ("bgVolume");
+        fxVolume = PlayerPrefs.GetFloat ("fxVolume");
+        bool.TryParse(PlayerPrefs.GetString ("Mute"),out mute);
+
+        bgAudio.volume = bgVolume;
+    }
+
+    void OnDisable() {
+        //Aqui salvo
+        PlayerPrefs.SetFloat ("bgVolume", bgVolume);
+        PlayerPrefs.SetFloat ("fxVolume", fxVolume);
+        PlayerPrefs.SetString ("Mute", mute.ToString());
+
+    }
+
+    public void onBGValueChangue(float value){
+        bgVolume = value;
+
+        bgAudio.volume = bgVolume;
+
+    }
+    public void onFXValueChangue(float value){
+        fxVolume = value;
+    }
+}
+
