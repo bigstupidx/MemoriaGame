712769ee400e892f925ba3adab80b90b58c724a0
Luis Alejandro Vieira <lavz24@gmail.com>
2014-09-04 12:12:10 -0430

    Agregado script para hacer link del material con ngui

---
+++ Assets/MemoriaGame/Scripts/GUI/LinkWidgetToMesh.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LinkWidgetToMesh : MonoBehaviour {
+
+    public UIWidget widget;
+    protected Material mat;
+    protected Renderer mesh;
+    // Use this for initialization
+    void Awake () {
+        mesh = GetComponent<MeshRenderer>();
+        if (mesh == null)
+            mesh = GetComponent<SkinnedMeshRenderer> ();
+        mat = mesh.material;
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        mat.SetFloat("_Alpha",widget.finalAlpha);
+        if(widget.finalAlpha <= 0){
+            if(mesh.enabled)
+                mesh.enabled = false;
+        }else if( !mesh.enabled){
+            mesh.enabled = true;
+        }
+    }
+}
