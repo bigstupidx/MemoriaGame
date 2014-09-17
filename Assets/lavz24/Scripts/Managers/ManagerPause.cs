1314b5e0616de30cae380b7c02e434f653e9b00d
Luis Alejandro Vieira <lavz24@gmail.com>
2014-09-17 14:22:52 -0430

    Document ManagerPause

--- Assets/lavz24/Scripts/Managers/ManagerPause.cs
+++ Assets/lavz24/Scripts/Managers/ManagerPause.cs
@@ -12,36 +12,53 @@ using System.Collections.Generic;
 
 public class ManagerPause : Singleton<ManagerPause> {
 
-
+    #region Paused
     public delegate void onPauseGameBroadcast();
     public event onPauseGameBroadcast OnPauseGame;
 
+    /// <summary>
+    /// Subscribes funct to OnPauseGame.
+    /// </summary>
+    /// <param name="funct">Funct.</param>
     public static void SubscribeOnPauseGame(onPauseGameBroadcast funct){
         Instance.OnPauseGame+=funct;
     }
+    /// <summary>
+    /// Unsubscribe safetely funct Function to OnPauseGameDelegate
+    /// </summary>
+    /// <param name="funct">Function.</param>
     public static void UnSubscribeOnPauseGame(onPauseGameBroadcast funct){
         if(Instance != null)
             Instance.OnPauseGame-=funct;
     }
+    #endregion
 
+    #region Resume:
     public delegate void onResumeGameBroadcast();
     public event onResumeGameBroadcast OnResumeGame;
 
+    /// <summary>
+    /// Subscribes funct to OnResumeGame
+    /// </summary>
+    /// <param name="funct">Funct.</param>
     public static void SubscribeOnResumeGame(onResumeGameBroadcast funct){
         Instance.OnResumeGame+=funct;
     }
+    /// <summary>
+    /// Unsubscribe safetely funct Function to OnResumeGameDelegate
+    /// </summary>
+    /// <param name="funct">Function.</param>
     public static void UnSubscribeOnResumeGame(onResumeGameBroadcast funct){
         if(Instance != null)
             Instance.OnResumeGame-=funct;
     }
-  /*  [HideInInspector]
-    public List<Signal> onGamePaused = new List<Signal>();*/
- /*   [HideInInspector]
-    public List<Signal> onGameResumed = new List<Signal>();
-    */
-
+    #endregion
 
 	private  bool paused = false;
+    /// <summary>
+    /// Gets or sets a value indicating whether this <see cref="ManagerPause"/> is pause.
+    /// </summary>
+    /// <value><c>true</c> if pause; otherwise, <c>false</c>.</value>
 	public  bool Pause {
 		get{
 
