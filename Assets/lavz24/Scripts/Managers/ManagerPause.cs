89d877af94f651ba4d4ac26be49f0f2a8470a6b9
Luis Alejandro Vieira <lavz24@gmail.com>
2014-09-17 11:54:26 -0430

    Fixed bug for unityExecutioon in Pause

--- Assets/lavz24/Scripts/Managers/ManagerPause.cs
+++ Assets/lavz24/Scripts/Managers/ManagerPause.cs
@@ -16,9 +16,24 @@ public class ManagerPause : Singleton<ManagerPause> {
     public delegate void onPauseGameBroadcast();
     public event onPauseGameBroadcast OnPauseGame;
 
+    public static void SubscribeOnPauseGame(onPauseGameBroadcast funct){
+        Instance.OnPauseGame+=funct;
+    }
+    public static void UnSubscribeOnPauseGame(onPauseGameBroadcast funct){
+        if(Instance != null)
+            Instance.OnPauseGame-=funct;
+    }
 
     public delegate void onResumeGameBroadcast();
     public event onResumeGameBroadcast OnResumeGame;
+
+    public static void SubscribeOnResumeGame(onResumeGameBroadcast funct){
+        Instance.OnResumeGame+=funct;
+    }
+    public static void UnSubscribeOnResumeGame(onResumeGameBroadcast funct){
+        if(Instance != null)
+            Instance.OnResumeGame-=funct;
+    }
   /*  [HideInInspector]
     public List<Signal> onGamePaused = new List<Signal>();*/
  /*   [HideInInspector]
@@ -28,8 +43,8 @@ public class ManagerPause : Singleton<ManagerPause> {
 
 	private  bool paused = false;
 	public  bool Pause {
-	
 		get{
+
 			return paused;
 		}
 		set{
@@ -41,5 +56,7 @@ public class ManagerPause : Singleton<ManagerPause> {
 			}
 		}
 	}
+
+
 
 }
