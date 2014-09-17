85ffdda81885d07a784b8237085ee57e138e8866
Luis Alejandro Vieira <lavz24@gmail.com>
2014-09-17 10:29:17 -0430

    Added new delegate system to pause

--- Assets/lavz24/Scripts/Managers/ManagerPause.cs
+++ Assets/lavz24/Scripts/Managers/ManagerPause.cs
@@ -13,10 +13,18 @@ using System.Collections.Generic;
 public class ManagerPause : Singleton<ManagerPause> {
 
 
-    [HideInInspector]
-    public List<Signal> onGamePaused = new List<Signal>();
-    [HideInInspector]
+    public delegate void onPauseGameBroadcast();
+    public event onPauseGameBroadcast OnPauseGame;
+
+
+    public delegate void onResumeGameBroadcast();
+    public event onResumeGameBroadcast OnResumeGame;
+  /*  [HideInInspector]
+    public List<Signal> onGamePaused = new List<Signal>();*/
+ /*   [HideInInspector]
     public List<Signal> onGameResumed = new List<Signal>();
+    */
+
 
 	private  bool paused = false;
 	public  bool Pause {
@@ -27,31 +35,11 @@ public class ManagerPause : Singleton<ManagerPause> {
 		set{
 			paused = value;
 			if(paused){
-				OnPauseGame();
+                OnPauseGame();
 			}else{
-				OnResumeGame();
+                OnResumeGame();
 			}
 		}
 	}
-	protected  void OnPauseGame(){
-        foreach (Signal sig in onGamePaused) {
 
-            sig.Invoke ();
-        }
-	}
-	protected  void OnResumeGame(){
-        foreach (Signal sig in onGameResumed) {
-
-            sig.Invoke ();
-        }
-	}
-
-	public  void PauseGame(){
-
-		Pause = !Pause;
-	}
-	public  void PauseGame(bool value){
-		
-		Pause = value;
-	}
 }
