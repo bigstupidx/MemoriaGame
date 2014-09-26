f3bc4d04cb87404c9caf628160c510a22c30d865
Luis Alejandro Vieira <lavz24@gmail.com>
2014-09-26 17:23:13 -0430

    Improve slice Feedback

--- Assets/MemoriaGame/Scripts/GUI/SetTweenForResolution.cs
+++ Assets/MemoriaGame/Scripts/GUI/SetTweenForResolution.cs
@@ -6,10 +6,25 @@ public class SetTweenForResolution : MonoBehaviour {
 
     public Transform StartPos;
     public Transform EndPos;
+
+    public Sprite normal;
+    public Sprite touch;
+
+    public UI2DSprite widget;
 	// Use this for initialization
 	void Awake () {
         GetComponent<TweenPosition> ().from = StartPos.localPosition;
         GetComponent<TweenPosition> ().to = EndPos.localPosition;
     }
 	
+
+    public void ChangueSpriteToTouch(){
+        widget.sprite2D = touch;
+    }
+
+
+    public void ChangueSpriteToNormal(){
+        widget.sprite2D = normal;
+
+    }
 }
