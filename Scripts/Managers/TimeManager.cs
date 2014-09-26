a95ccca286746b866065f1904432ef1c506ff559
Luis Alejandro Vieira <lavz24@gmail.com>
2014-09-26 09:04:17 -0430

    Squashed 'Assets/lavz24/' changes from 5261cc1..6b33b1b
    6b33b1b Agregado TimeManager
    
    git-subtree-dir: Assets/lavz24
    git-subtree-split: 6b33b1bc3c78621a53a5d005481735fc98a96b2b

---
+++ Scripts/Managers/TimeManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class TimeManager : Singleton<TimeManager>
+{
+    public IEnumerator Wait(float duration)
+    {
+
+
+        for (float timer = 0; timer < duration; timer += Time.deltaTime)
+        {
+
+            yield return null;
+        }
+    }
+    public IEnumerator WaitPause(float duration)
+    {
+        if (ManagerPause.Instance.Pause)
+            yield return null;
+
+        for (float timer = 0; timer < duration; timer += Time.deltaTime)
+        {
+ //           if (ManagerPause.Instance.Pause)
+  //              yield return null;
+
+            yield return null;
+        }
+    }
+}
