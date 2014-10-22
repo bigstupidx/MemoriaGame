a1c0f393224c379e46f0f789afe52aae60e333cf
Luis Alejandro Vieira <lavz24@gmail.com>
2014-10-22 17:16:26 -0430

    AddedSmartLocalization

---
+++ Assets/SmartLocalization/NGUI/Editor/SL_NGUISprite2DNameInspector.cs
@@ -0,0 +1,43 @@
+﻿// SL_NGUISpriteNameInspector.cs
+//
+// Copyright (c) 2013-2014 Niklas Borglund, Jakob Hillerström
+//
+
+//#define SMART_LOC_NGUI //<--- UNCOMMENT THIS FOR NGUI CLASSES
+
+#if SMART_LOC_NGUI
+namespace SmartLocalization.Editor
+{
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(SL_NGUISpriteName))]
+public class SL_NGUISprite2DNameInspector : Editor 
+{
+private string selectedKey = null;
+
+void Awake()
+{
+SL_NGUISpriteName textObject = ((SL_NGUISpriteName)target);
+if(textObject != null)
+{
+selectedKey = textObject.localizedKey;
+}
+}
+
+public override void OnInspectorGUI()
+{
+base.OnInspectorGUI();
+
+selectedKey = LocalizedKeySelector.SelectKeyGUI(selectedKey, true, LocalizedObjectType.STRING);
+
+if(!Application.isPlaying && GUILayout.Button("Use Key", GUILayout.Width(70)))
+{
+SL_NGUISpriteName spriteObject = ((SL_NGUISpriteName)target);       
+spriteObject.localizedKey = selectedKey;
+}
+}
+}
+} //namespace SmartLocalization.Editor
+#endif
+
