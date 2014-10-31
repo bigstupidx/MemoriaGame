496228a2d34ee95eaae8ceec592d69e78805ffcc
Luis Alejandro Vieira <lavz24@gmail.com>
2014-10-31 16:21:54 -0430

    fixed animation or games

---
+++ Assets/MemoriaGame/Scripts/Utils/QualitySet.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QualitySet : MonoBehaviour {
+
+    #if UNITY_IPHONE
+    void Awake(){
+
+        if( iPhone.generation == iPhoneGeneration.iPhone4)
+        {
+            QualitySettings.SetQualityLevel (0);
+        }
+
+    }
+    #endif
+	
+
+}
