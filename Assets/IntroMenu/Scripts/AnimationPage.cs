c73d47093e6ce1d45bea2f1b1ecada5a100331af
Luis Alejandro Vieira <lavz24@gmail.com>
2014-11-21 14:48:01 -0430

    Casi listo el intro

--- Assets/IntroMenu/Scripts/AnimationPage.cs
+++ Assets/IntroMenu/Scripts/AnimationPage.cs
@@ -8,14 +8,18 @@ public class AnimationPage : MonoBehaviour {
     public UISpriteAnimationLimit left;
     public UISpriteAnimationLimit right;
 
+
+    public UISpriteAnimation Title;
 	// Update is called once per frame
     public void ToLeft () {
+        Title.gameObject.SetActive(false);
 
 
         left.PlayForward();
 
 	}
     public void ToRight () {
+        Title.gameObject.SetActive(false);
 
         right.PlayForward();
 
@@ -23,7 +27,6 @@ public class AnimationPage : MonoBehaviour {
     }
 
     public void ShutDown(){
-
         gameObject.SetActive(false);
 
         sprite.spriteName = spriteName;
@@ -32,5 +35,9 @@ public class AnimationPage : MonoBehaviour {
     public void TurnOn(){
         sprite.spriteName = spriteName;
         gameObject.SetActive(true);
+
+        Title.gameObject.SetActive(true);
+        Title.enabled = true;
+        Title.ResetToBeginning();
     }
 }
